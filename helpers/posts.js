82c7cc109080d910c9f6a87fb5aeeb7d950697e7
Steve Carroll <scarroll531@gmail.com>
2015-09-30 22:28:18 -0400

    remove extraneous code from posts.js

--- helpers/posts.js
+++ helpers/posts.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var path = require('path');
-// var manifest = require('../posts/manifest.json');
 var _scope = this;
 
 var PostHelper = function() {
@@ -17,44 +16,16 @@ var PostHelper = function() {
 				fs.read(fd, buffer, 0, buffer.length, null, function(err, bytesRead, buffer) {
 					var data = buffer.toString('utf8', 0, buffer.length);
 
-					_scope.manifest = JSON.parse(data);
 					fs.close(fd);
 
 					if (callback) {
-						callback(_scope.manifest);
+						callback(JSON.parse(data));
 					}
 					
 				});
 			});
 		});
-
-		function returnPosts() {
-			return JSON.parse(_scope.manifest);
-		}
-		
-		// fs.open(path.join(__dirname, '/../posts/manifest.json'), 'r+', function(err, fd) {
-		// 	if (err) throw err;
-		// 	console.log(fd);
-		// });
-
-		// function readFile(err, fd) {
-		// 	if (err) throw err;
-		// }
-
 	};
-
-	// getPosts();
 };
-
-// console.log(manifest);
-// manifest.posts.push({
-// 	'title': 'second',
-// 	'description': 'this is the second post, bro',
-// 	'filename': 'third.md'
-// });
-
-// console.log(manifest);
-
-// var postHelper = new PostHelper();
 
 module.exports = PostHelper;
